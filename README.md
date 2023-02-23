4cb5f744df7db0d336f629498a8fc5f7c8daf599
Jatin Kumar <main.jatink@gmail.com>
2023-02-24 00:26:05 +0530

    chore: base vite + react template

--- README.md
+++
@@ -1,2 +0,0 @@
-# SCCWebsite
-club website
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import About from "./components/about/About";
+import Home from "./components/home/Home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
