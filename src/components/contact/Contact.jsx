f80820fd97110b04e4dbed130dcf31148cd6f390
tanmayagrwl <tanmayhp78@gmail.com>
2023-02-25 01:53:16 +0530

    add projects and contact section

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+    return (
+      <div>
+        <h1 className="text-3xl font-medium">This is Contact</h1>
+      </div>
+    );
+  }
+  
+  export default Contact;
+  
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,10 @@
+function Projects() {
+    return (
+      <div>
+        <h1 className="text-3xl font-medium">This is Projects</h1>
+      </div>
+    );
+  }
+  
+  export default Projects;
+  
