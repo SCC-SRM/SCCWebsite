8dbcc47d2119825eb14be75ff993b4c7c9652491
tanmayagrwl <tanmayhp78@gmail.com>
2023-02-26 16:09:55 +0530

    create grid

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,27 @@
 function Contact() {
     return (
       <div>
-        <h1 className="text-3xl font-medium">This is Contact</h1>
+        {/* creating grid   */}
+        <div className="grid grid-cols-2"> 
+
+
+          {/* first box */}
+          <div className="bg-red-400 h-[35rem]">
+            <h1 className="text-[3rem]">Contact</h1>
+          </div> 
+
+
+          {/* second box */}
+          <div className="bg-green-400 row-span-2">02</div>
+
+
+
+          {/* third box */}
+          <div className="bg-yellow-400 h-[5rem]">03</div>   
+          
+
+
+        </div>
       </div>
     );
   }
