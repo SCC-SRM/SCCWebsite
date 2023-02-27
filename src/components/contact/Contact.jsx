08f0f200c18f09dfb0395e64501e36163f47c705
tanmayagrwl <tanmayhp78@gmail.com>
2023-02-27 22:01:20 +0530

    add social logos + create page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,30 +1,57 @@
+import React from "react";
+import Facebook from './logo-facebook.svg'
+import Instagram from './logo-instagram.svg'
+import Linkedin from './logo-linkedin.svg'
+import Twitter from './logo-twitter.svg'
+import Youtube from './logo-youtube.svg'
+
+
 function Contact() {
-    return (
-      <div>
-        {/* creating grid   */}
-        <div className="grid grid-cols-2"> 
+  return (
+   <div className="bg-gray-500">
+    <div className="flex w-full min-h-screen justify-center items-center">
+    <div className="flex flex-col space-y-6 bg-gray-600 w-full max-w-6xl p-8 rounded-xl shadow-lg md:flex-row md:space-x-6 md:space-y-0 justify-between sm:p-12">
+      <div className="flex flex-col justify-between mx-10 ">
+          <div>
+            <h1 className="font-bold text-3xl tracking-wide ">Contact Us</h1>
+            <p className="pt-2">For general enquiry, event or press, please drop us a message</p>
+          </div>
 
+          <div className="flex space-x-4 my-10">
+            <a href="#"><img src={Facebook} width="25" alt="" /> </a>
+            <a href="#"><img src={Instagram} width="25" alt="" /></a>
+            <a href="#"><img src={Twitter} width="25" alt="" /></a>
+            <a href="#"><img src={Youtube} width="25" alt="" /></a>
+            <a href="#"><img src={Linkedin} width="25" alt="" /></a>
 
-          {/* first box */}
-          <div className="bg-red-400 h-[35rem]">
-            <h1 className="text-[3rem]">Contact</h1>
-          </div> 
-
-
-          {/* second box */}
-          <div className="bg-green-400 row-span-2">02</div>
-
-
-
-          {/* third box */}
-          <div className="bg-yellow-400 h-[5rem]">03</div>   
-          
-
-
-        </div>
+          </div>
       </div>
-    );
-  }
-  
-  export default Contact;
+
+
+      <div>
+          <div className="bg-white rounded-xl shadow-lg p-8 w-full md:w-80">
+            <form action="" className="flex flex-col space-y-4 w-full">
+              <div><label htmlFor="" className="text-sm">Your Name</label><input type="text" placeholder="Your Name" className="ring-1 ring-black w-full rounded-md px-4 py-2 outline-none mt-2"/></div>
+
+
+            
+              <div><label htmlFor="" className="text-sm">Your Email</label><input type="email" placeholder="Your Email " className="ring-1 ring-black w-full rounded-md px-4 py-2 outline-none mt-2"/></div>
+              
+              <div><label htmlFor="" className="text-sm">Message</label><textarea type="text" placeholder="message" rows="4" className="ring-1 ring-black w-full rounded-md px-4 py-2 outline-none mt-2"/></div>
+
+              <button className="inline-block self-end bg-black
+               text-white rounded-lg font-bold  py-4 px-6 uppercase text-sm">Send Message</button>
+
+            </form>
+            </div>
+          </div>
+      </div>
+    </div>
+      
+   </div>
+   
+  );
+}
+
+export default Contact;
   
