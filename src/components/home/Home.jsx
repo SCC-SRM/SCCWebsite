2f4af5b8cc164854bfdc2d0d1d40f87dcaf3162f
nitinagarwal2002 <agarwalnitin1001@gmail.com>
2023-03-12 19:50:03 +0530

    solved error in home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,5 @@
 function Home() {
   return (
-
-    <div className="header absolute top-0 left-0 flex items-center">
-      <h1 className="text-2xl font-bold">Upcoming Events</h1>
-      <div className="w-4 h-1 ml-2 bg-gray-500"></div>
-
     <div className="min-h-full flex flex-col px-20 py-8 bg-[#1d1d1d] text-white">
       <nav className="flex justify-between font-semibold mb-6 h-full tracking-wide">
         <div className="inline-flex gap-10 items-center">
@@ -56,7 +51,6 @@ function Home() {
           </button>
         </div>
       </section>
-
     </div>
   );
 }
