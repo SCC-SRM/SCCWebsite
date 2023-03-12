66a2f60121f5cf2ab6aedc8007922787251fe4ed
nitinagarwal2002 <agarwalnitin1001@gmail.com>
2023-03-12 14:29:26 +0530

    update app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-// import About from "./components/about/About";
-// import Home from "./components/home/Home";
-// import Contact from "./components/contact/Contact";
-// import Projects from "./components/projects/Projects";
+import About from "./components/about/About";
+import Home from "./components/home/Home";
+import Contact from "./components/contact/Contact";
+import Projects from "./components/projects/Projects";
 import Events from "./components/events/Events";
 
 function App() {
   return (
     <div>
-      {/* <Home /> */}
+      <Home />
       <Events/>
-      {/* <About />
+      <About />
       <Projects />
-      <Contact /> */}
+      <Contact />
     </div>
   );
 }
