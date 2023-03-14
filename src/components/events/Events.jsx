e9ab6b093357d1668e82f5fea852c17def762ead
nitinagarwal2002 <agarwalnitin1001@gmail.com>
2023-03-14 23:23:36 +0530

    change the layout

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -3,94 +3,118 @@ import "./index.css";
 function Events() {
   return (
     <>
-      <div className="header absolute top-0 right-2 flex items-center">
-        <h1 className="text-4xl font-bold mr-auto text-custom1">
-          Upcoming Events
-        </h1>
-      </div>
-      <div className="absolute top-8 right-0 w-1/4 h-10 border-b-4 z-10 border-custommonth"></div>
-      <div className="relative p-4 top-20">
-        <span
-          className="font-helvetica font-bold text-9xl text-custommonth md:text-11xl lg:text-14xl xl:text-16xl 2xl:text-20xl relative"
-          style={{ textShadow: "2px 2px 2px grey", fontSize: "10rem" }}
-        >
-          Jan
-        </span>
-        <span
-          className="font-helvetica font-bold text-9xl text-custom1 md:text-11xl lg:text-14xl xl:text-16xl 2xl:text-20xl absolute top-20 left-12 translate-x-1/2"
-          style={{ textShadow: "2px 2px 2px grey", fontSize: "10rem" }}
-        >
-          23'
-        </span>
-      </div>
+      <div className="min-h-full mb-20">
+        <div className="flex p-0">
+          <span className="font-bold text-4xl text-underline-offset: 8px underline decoration-[#F18C00] text-custom1 ml-auto ">
+            Upcoming Events
+          </span>
+        </div>
 
-      <div className="marg absolute top-80 gap-y-10 grid grid-cols-2  gap-20 pr-50">
-        <span className="font-helvetica">
-          <p className="font-helvetica font-bold text-4xl text-custommonth ">
-            15th JANUARY <br />
-          </p>
-          <p className="font-helvetica font-bold text-xl text-custom1">
-            4:00PM IST <br />
-          </p>
-          <p className="font-helvetica font-bold text-xl text-custom1 ">
-            HACKATHON <br />
-          </p>
-        </span>
-        <span className="font-helvetica">
-          <p className="font-helvetica font-bold text-4xl text-custommonth ">
-            23rd JANUARY <br />
-          </p>
-          <p className="font-helvetica font-bold text-xl text-custom1">
-            4:00PM IST <br />
-          </p>
-          <p className="font-helvetica font-bold text-xl text-custom1 ">
-            HACKATHON <br />
-          </p>
-        </span>
-        <span className="font-helvetica">
-          <p className="font-helvetica font-bold text-4xl text-custommonth ">
-            19th JANUARY <br />
-          </p>
-          <p className="font-helvetica font-bold text-xl text-custom1">
-            4:00PM IST <br />
-          </p>
-          <p className="font-helvetica font-bold text-xl text-custom1 ">
-            HACKATHON <br />
-          </p>
-        </span>
-        <span className="font-helvetica">
-          <p className="font-helvetica font-bold text-4xl text-custommonth ">
-            27th JANUARY <br />
-          </p>
-          <p className="font-helvetica font-bold text-xl text-custom1">
-            4:00PM IST <br />
-          </p>
-          <p className="font-helvetica font-bold text-xl text-custom1 ">
-            HACKATHON <br />
-          </p>
-        </span>
-        <span className="font-helvetica">
-          <p className="font-helvetica font-bold text-4xl text-custommonth ">
-            21st JANUARY <br />
-          </p>
-          <p className="font-helvetica font-bold text-xl text-custom1">
-            10:00AM IST <br />
-          </p>
-          <p className="font-helvetica font-bold text-xl text-custom1 ">
-            HACKATHON <br />
-          </p>
-        </span>
-        <span className="font-helvetica">
-          <p className="font-helvetica font-bold text-4xl text-custommonth ">
-            31st JANUARY <br />
-          </p>
-          <p className="font-helvetica font-bold text-xl text-custom1">
-            10:00AM IST <br />
-          </p>
-          <p className="font-helvetica font-bold text-xl text-custom1 ">
-            HACKATHON <br />
-          </p>
-        </span>
+        <div className="relative p-4 top-5">
+          <span
+            className="font-helvetica font-bold  text-custommonth absolute"
+            style={{ textShadow: "2px 2px 2px grey", fontSize: "10rem" }}
+          >
+            JAN
+          </span>
+          <span
+            className="font-helvetica font-bold  text-custom1  absolute top-20 left-12 translate-x-1/2"
+            style={{ textShadow: "2px 2px 2px grey", fontSize: "10rem" }}
+          >
+            23'
+          </span>
+        </div>
+        <div className="flex justify-center items-center">
+          <div
+            className="relative top-80 gap-y-10 grid grid-cols-2 gap-x-20 "
+            style={{ maxWidth: "fit-content" }}
+          >
+            <span
+              className="font-helvetica inline-block w-auto justify-self-center"
+              style={{ maxWidth: "fit-content" }}
+            >
+              <p className="font-helvetica font-bold text-3xl text-custommonth">
+                15th JANUARY <br />
+              </p>
+              <p className="font-helvetica font-bold text-xl text-custom1">
+                4:00PM IST <br />
+              </p>
+              <p className="font-helvetica font-bold text-xl text-custom1">
+                HACKATHON <br />
+              </p>
+            </span>
+            <span
+              className="font-helvetica inline-block w-auto"
+              style={{ maxWidth: "fit-content" }}
+            >
+              <p className="font-helvetica font-bold text-3xl text-custommonth">
+                23rd JANUARY <br />
+              </p>
+              <p className="font-helvetica font-bold text-xl text-custom1">
+                4:00PM IST <br />
+              </p>
+              <p className="font-helvetica font-bold text-xl text-custom1">
+                HACKATHON <br />
+              </p>
+            </span>
+            <span
+              className="font-helvetica inline-block w-auto justify-self-center"
+              style={{ maxWidth: "fit-content" }}
+            >
+              <p className="font-helvetica font-bold text-3xl text-custommonth">
+                15th JANUARY <br />
+              </p>
+              <p className="font-helvetica font-bold text-xl text-custom1">
+                4:00PM IST <br />
+              </p>
+              <p className="font-helvetica font-bold text-xl text-custom1">
+                HACKATHON <br />
+              </p>
+            </span>
+            <span
+              className="font-helvetica inline-block w-auto"
+              style={{ maxWidth: "fit-content" }}
+            >
+              <p className="font-helvetica font-bold text-3xl text-custommonth">
+                23rd JANUARY <br />
+              </p>
+              <p className="font-helvetica font-bold text-xl text-custom1">
+                4:00PM IST <br />
+              </p>
+              <p className="font-helvetica font-bold text-xl text-custom1">
+                HACKATHON <br />
+              </p>
+            </span>
+            <span
+              className="font-helvetica inline-block w-auto justify-self-center"
+              style={{ maxWidth: "fit-content" }}
+            >
+              <p className="font-helvetica font-bold text-3xl text-custommonth">
+                15th JANUARY <br />
+              </p>
+              <p className="font-helvetica font-bold text-xl text-custom1">
+                4:00PM IST <br />
+              </p>
+              <p className="font-helvetica font-bold text-xl text-custom1">
+                HACKATHON <br />
+              </p>
+            </span>
+            <span
+              className="font-helvetica inline-block w-auto"
+              style={{ maxWidth: "fit-content" }}
+            >
+              <p className="font-helvetica font-bold text-3xl text-custommonth">
+                23rd JANUARY <br />
+              </p>
+              <p className="font-helvetica font-bold text-xl text-custom1">
+                4:00PM IST <br />
+              </p>
+              <p className="font-helvetica font-bold text-xl text-custom1">
+                HACKATHON <br />
+              </p>
+            </span>
+          </div>
+        </div>
       </div>
     </>
   );
