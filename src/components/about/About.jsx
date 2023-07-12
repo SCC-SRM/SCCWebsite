ca8edb60dce5fa361d295cf86ed7a44cf6f0886c
anuragrege <anuragrege@gmail.com>
2023-07-12 19:41:58 +0530

    make history section responsive

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -36,38 +36,36 @@ function About() {
           </p>
         </div>
       </div>
-      <div className="min-h-full bg-primaryBackground text-white px-20 py-16 space-y-20">
+      <div className="min-h-screen bg-primaryBackground text-white pt-32 space-y-20">
         <div>
           <span className="font-bold text-6xl underline decoration-[#d97706]">
             HISTORY
           </span>
         </div>
-        <div className="flex flex-col lg:flex-row">
-          <div className="flex flex-col justify-center lg:w-1/2">
-            <p className="text-lg mb-4">
-              The club was created with the goal of providing students with a
-              community where they can learn and develop their skills in
-              programming and app development. The club is open to students of
-              all skill levels, from those who are just starting out to those
-              who have been coding for years.
-            </p>
-            <p className="text-lg">
-              The club was inaugurated and was made open to the students of SRM
-              to join on **XYZ date**. Ever since then, we have been striving to
-              create new opportunities for our club members to grow in all
-              aspects that would help them become better professionals in the
-              various fields they would like to have a career in. The club has
-              grown from a group of X no. of people to y no. (the present
-              strength) of people.
-            </p>
-          </div>
-          <figure className="relative max-w-7xl cursor-pointer filter lg:w-1/2">
+
+        <div className="flex flex-col justify-center lg:ml-4">
+          <p className="text-lg">
+            The club was created with the goal of providing students with a
+            community where they can learn and develop their skills in
+            programming and app development. The club is open to students of all
+            skill levels, from those who are just starting out to those who have
+            been coding for years. The club was inaugurated and was made open to
+            the students of SRM to join on **XYZ date**. Ever since then, we
+            have been striving to create new opportunities for our club members
+            to grow in all aspects that would help them become better
+            professionals in the various fields they would like to have a career
+            in. The club has grown from a group of X no. of people to y no. (the
+            present strength) of people.
+          </p>
+        </div>
+        <div className="flex flex-col lg:flex-row justify-center">
+          <div className="lg:w-1/2">
             <img
-              className="rounded-lg max-w-full ml-64 mt-5 transition-all duration-300 grayscale hover:grayscale-0"
+              className="lg:mx-auto border-4 border-yellow-500 h-full w-full lg:overflow-hidden rounded-lg max-w-2xl ml-2 transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
               src="/about2.jpeg"
               alt="history image"
             />
-          </figure>
+          </div>
         </div>
       </div>
     </div>
