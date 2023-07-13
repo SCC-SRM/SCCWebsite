8af76f1d1d0c7e04e11c9b4e9b7fc2cdbfb0394d
Jatin Kumar <main.jatink@gmail.com>
2023-07-13 14:44:23 +0530

    chore: use better icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { Instagram, Linkedin, Twitter } from "lucide-react";
+import { Instagram, Linkedin, Mail, Phone, Twitter } from "lucide-react";
 
 function Footer() {
   const handleCopy = (e) => {
@@ -79,38 +79,15 @@ function Footer() {
                 onClick={handleCopy}
                 className="hover:text-white outline-none focus-within:text-white"
                 data-text="info@sccsrm.com">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-4 h-4 inline-block mr-1">
-                  <path
-                    strokeLinecap="round"
-                    d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25"
-                  />
-                </svg>
+                <Mail className="w-4 h-4 inline-block mr-1" />
                 info@sccsrm.com
               </button>
-              <span className="mx-1.5">·</span>
+              <span className="mx-1.5 select-none">·</span>
               <button
                 onClick={handleCopy}
                 className="hover:text-white outline-none focus-within:text-white"
                 data-text="+919929294949">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-4 h-4 inline-block mr-1">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
-                  />
-                </svg>
+                <Phone className="w-4 h-4 inline-block mr-1" />
                 +91 99292 94949
               </button>
             </div>
