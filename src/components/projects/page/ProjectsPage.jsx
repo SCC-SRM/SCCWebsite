b373e98c856dcdea518ba0d58e3204791c3320fb
ishaan <ishaankhurana123@gmail.com>
2023-07-26 23:39:38 +0530

    feat: changed text formatting and styles for projects page

--- src/components/projects/page/ProjectsPage.jsx
+++ src/components/projects/page/ProjectsPage.jsx
@@ -8,27 +8,30 @@ function ProjectsPage() {
     <div className="flex flex-col py-8 bg-primaryBackground text-white px-20">
       <div className=" bg-primaryBackground">
         <div className="py-16 pr-20">
-          <div className="flex mb-20">
-            <div className="mt-16 rotate-90">
-              <h1 className="font-bold text-7xl">PROJECTS</h1>
-              <div className="bg-[#ef4444] h-2 w-24 ml-auto mt-2 -mr-7"></div>
-            </div>
+          <h1 className="font-bold text-7xl">PROJECTS</h1>
+          <div className="bg-[#ef4444] h-2 w-96"></div>
+          <div className="flex mb-10">
+            <div className="mt-16"></div>
             <div>
-              <p className="text-lg ">
+              <p className="max-w-5xl mt-5 sm:text-lg text-lg lg:text-3xl  font-extralight pt-10 text-justify">
                 Swift coding club is all about hands-on learning and building
                 projects. Our projects cover a wide range of topics and
                 technologies, from beginner-friendly tutorials to more advanced
                 challenges. Whether you're just starting out or are already a
-                seasoned coder, we have something for everyone. Some of our
-                popular projects include:
+                seasoned coder,
+                <span className="text-[#ef4444]">
+                  {" "}
+                  we have something for everyone.{" "}
+                </span>
+                Some of our popular projects include:
               </p>
-              <ul className="pt-28">
+              {/* <ul className="pt-20 text-xl">
                 <li>- Building a simple website</li>
                 <li>- Creating a mobile app</li>
                 <li>- Developing a game</li>
                 <li>- Designing a machine learning model</li>
                 <li>- Building a chatbot</li>
-              </ul>
+              </ul> */}
             </div>
           </div>
         </div>
