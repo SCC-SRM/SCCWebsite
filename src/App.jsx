e440eb481cc400ee5c2f3916204dda79afa2df4d
Jatin <main.jatink@gmail.com>
2023-09-06 11:52:30 +0530

    feat: add events registration page (#30)
    * feat: add events registration
    
    * update contact details
    
    * fix redirects maybe
    
    * fix redirects maybe again

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ProjectsPage from "./components/projects/page/ProjectsPage";
-import Footer from "./components/footer/Footer";
-import LandingPage from "./LandingPage";
-import Domains from "./components/domains/Domains";
-import Nav from "./components/nav/Nav";
-import Events from "./components/events/Events";
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ProjectsPage from './components/projects/page/ProjectsPage'
+import Footer from './components/footer/Footer'
+import LandingPage from './LandingPage'
+import Domains from './components/domains/Domains'
+import Nav from './components/nav/Nav'
+import Events from './components/events/Events'
 
 function App() {
   return (
     <div className="min-h-full items-center bg-primaryBackground text-white">
-      <div className="flex flex-col items-center">
-        <img className="fixed top-9 mx-auto h-[42vw]" src="bg-swift.svg"></img>
-      </div>
       <BrowserRouter>
         <Nav />
         <Routes>
@@ -23,8 +20,11 @@ function App() {
         </Routes>
       </BrowserRouter>
       <Footer />
+      <div className="flex flex-col items-center">
+        <img className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mx-auto h-[42vw] pointer-events-none select-none" src="bg-swift.svg"></img>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
