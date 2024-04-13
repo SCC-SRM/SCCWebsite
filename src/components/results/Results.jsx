fa31f77577789cdaff57df099ce29c63ac944967
anuragrege <anuragrege@gmail.com>
2024-04-13 18:26:09 +0530

    add corporate team results

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -38,7 +38,7 @@ function Results() {
         >
           Technical
         </button>
-        <button
+        {/*<button
           onClick={() => handleDomainChange("creatives")}
           className={`px-4 py-2 text-lg font-semibold rounded-lg focus:outline-none ${
             selectedDomain === "creatives"
@@ -47,11 +47,11 @@ function Results() {
           }`}
         >
           Creatives
-        </button>
+        </button>*/}
       </div>
 
       {selectedDomain === "corporate" && (
-        <div className="mt-8">
+        <div className="mt-8  overflow-x-scroll projects-scrollbar">
           <h2 className="text-2xl font-semibold mb-4">Corporate Domain</h2>
           <table className="w-full border-collapse border border-white">
             <thead>
@@ -66,10 +66,316 @@ function Results() {
               <tr>
                 <td className="border border-white text-center px-4 py-2">1</td>
                 <td className="border border-white text-center px-4 py-2">
-                  123
+                  RA2211003011980
                 </td>
                 <td className="border border-white text-center px-4 py-2">
-                  ABC
+                  Aaditya Naidu
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">2</td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2311032010046
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Chandrakant Subheswar Subudhi
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">3</td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2211027010257
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Achal Bajpai
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">4</td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2211003011658
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Parv Lunia
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">5</td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2311026010111
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Hirav Kadikar
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">6</td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2311003010154
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Keshav Gupta
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">7</td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2211042010011
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Siddharth Dhadwal
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">8</td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2211028010009
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Shashwatt bhatt
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">9</td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2211003010910
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Sneha Gorai
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  10
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2311026010499
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Subhamaya Mohanty
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  11
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2311056010147
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Amodh Saxena
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  12
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2311003011239
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Hitesh Shimpi
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  13
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2211003012014
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Namit Kaul
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  14
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2311026010484
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Harsh
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  15
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2211030010025
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Raghavan M
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  16
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2311030010327
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Arijeet Pratap Singh
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  17
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2211030010169
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Utkarsh Nayan
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  18
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2211026010349
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Arav Goel
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  19
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2311033010120
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Anushka Gupta
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  20
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2211043010015
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Debanshu Roy
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  21
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2311005010060
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Harman Tej Singh Virk
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  22
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2312703010009
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Adarsh Abhilash
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  23
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2211003011534
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Anushka Pandey
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Corporate
+                </td>
+              </tr>
+              <tr>
+                <td className="border border-white text-center px-4 py-2">
+                  24
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  RA2211053010058
+                </td>
+                <td className="border border-white text-center px-4 py-2">
+                  Ishita Misra
                 </td>
                 <td className="border border-white text-center px-4 py-2">
                   Corporate
